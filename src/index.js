74d69bceafd139307d0b37d26092d9a21f2676d3
Haseeb <haseeb.sayed@sjsu.edu>
2024-08-30 21:12:32 -0700

    this is hamed - testing git ignore

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import "slick-carousel/slick/slick.css"; 
 import './index.css';
 import App from './App';
-
+//test
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
