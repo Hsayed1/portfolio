62c4124b809554bd468d6aa8081789abf6f3266f
Hamed Sayed <hamedsayed@Hameds-MacBook-Pro.local>
2025-11-09 16:27:12 -0800

    alt

--- src/components/projects/ProjectsCard.js
+++ src/components/projects/ProjectsCard.js
@@ -12,6 +12,7 @@ const ProjectsCard = ({ title, des, src, gitLink }) => {
         <img
           className="w-full h-60 object-contain group-hover:scale-110 duration-300 cursor-pointer"
           src={src}
+          alt={title}
         />
       </div>
       <div className="w-full mt-5 flex flex-col gap-6">
